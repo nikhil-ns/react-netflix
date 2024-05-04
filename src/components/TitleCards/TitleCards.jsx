97ab14406cb3659338f1ae518cf71cc22286c7cb
nikhil-ns <nikhiladi19@gmail.com>
2024-05-04 13:52:12 +0530

    hero section

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import "./TitleCards.css"
+import cards_data from "../../assets/cards/Cards_data"
+import { useRef } from 'react'
+import { useEffect } from 'react';
+
 
 const TitleCards = () => {
+
+// const cardsRef = useRef();
+
+// const handleWheel = (event)=>{
+//   event.preventDefault();
+//   cardsRef.current.scollLeft += event.deltaY;
+// };
+
+// useEffect(()=>{
+//   cardsRef.current.addEventListener("wheel", handleWheel);
+// }, [])
+
   return (
-    <div className='titlecards'>
-      
+    <div className='title-cards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list"> {/* ref={cardsRef} */}
+        {cards_data.map((card, index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
